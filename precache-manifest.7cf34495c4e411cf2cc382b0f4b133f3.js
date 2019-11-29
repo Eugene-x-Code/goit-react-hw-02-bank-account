99414be248448d1939d09f1ef176ea552e78c584
Eugene-x-Code <evgeni.iv.xy@gmail.com>
2019-11-30 01:37:53 +0200

    Updates

---
+++ precache-manifest.7cf34495c4e411cf2cc382b0f4b133f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c30c1f7b1bd9d5bb0ce1164b6dba03f",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "50c58662020d7a309173",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "401d654d63ded3f42b43",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.5c631138.chunk.css"
+  },
+  {
+    "revision": "50c58662020d7a309173",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.c180d847.chunk.js"
+  },
+  {
+    "revision": "401d654d63ded3f42b43",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.41fa6875.chunk.js"
+  },
+  {
+    "revision": "59b69276754240abb686",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.611f8d42.js"
+  }
+]);
