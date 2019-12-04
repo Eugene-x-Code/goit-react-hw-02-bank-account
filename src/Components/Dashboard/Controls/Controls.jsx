d9d46b284b817619acda8537ae0ac8abf406c231
Eugene-x-Code <evgeni.iv.xy@gmail.com>
2019-12-05 01:57:27 +0200

    fix fix fix

--- src/Components/Dashboard/Controls/Controls.jsx
+++ src/Components/Dashboard/Controls/Controls.jsx
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './controls.module.scss';
 
-const Controls = ({ createTransaction, handleInput }) => (
-  <section className={css.controls}>
-    <form onSubmit={e => e.preventDefault()}>
-      <input
-        type="number"
-        name="amount"
-        onChange={e => handleInput(e.target.value)}
-      />
-      <button type="button" name="deposit" onClick={createTransaction}>
-        Deposit
-      </button>
-      <button type="button" name="withdraw" onClick={createTransaction}>
-        Withdraw
-      </button>
-    </form>
-  </section>
-);
+class Controls extends Component {
+  state = { input: '' };
 
-Controls.propTypes = {
-  createTransaction: PropTypes.func.isRequired,
-  handleInput: PropTypes.func.isRequired,
-};
+  static propTypes = {
+    createTransaction: PropTypes.func.isRequired,
+  };
+
+  onInputChange(e) {
+    this.setState({ input: e.target.value });
+  }
+
+  render() {
+    const { createTransaction } = this.props;
+    const { input } = this.state;
+    return (
+      <section className={css.controls}>
+        <form onSubmit={e => e.preventDefault()}>
+          <input
+            type="number"
+            name="amount"
+            onChange={e => this.onInputChange(e)}
+          />
+          <button
+            type="button"
+            name="deposit"
+            onClick={e => createTransaction(e.target, input)}
+          >
+            Deposit
+          </button>
+          <button
+            type="button"
+            name="withdraw"
+            onClick={e => createTransaction(e.target, input)}
+          >
+            Withdraw
+          </button>
+        </form>
+      </section>
+    );
+  }
+}
 
 export default Controls;
--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -11,37 +11,10 @@ class Dashboard extends Component {
   state = {
     transactions: [],
     balance: 0,
-    input: 0,
   };
 
-  componentDidMount() {
-    const presistedTransactions = localStorage.getItem('transactions');
-    const presistedBalance = localStorage.getItem('balance');
-
-    if (presistedTransactions) {
-      this.setState({
-        transactions: JSON.parse(presistedTransactions),
-        balance: JSON.parse(presistedBalance),
-      });
-    }
-  }
-
-  componentDidUpdate(prevState) {
-    const { transactions, balance } = this.state;
-    if (prevState.transactions !== transactions) {
-      localStorage.setItem('transactions', JSON.stringify(transactions));
-    }
-    if (prevState.balance !== balance) {
-      localStorage.setItem('balance', JSON.stringify(balance));
-    }
-  }
-
-  handleInput = query => {
-    this.setState({ input: query });
-  };
-
-  createTransaction = ({ target }) => {
-    const { balance, input } = this.state;
+  createTransaction = (target, input) => {
+    const { balance } = this.state;
     const inputVal = Number(input);
     target.parentNode.reset();
     if (checkCorrectInput(inputVal, target.name, balance)) {
@@ -76,10 +49,7 @@ class Dashboard extends Component {
     const { transactions, balance } = this.state;
     return (
       <div>
-        <Controls
-          createTransaction={this.createTransaction}
-          handleInput={this.handleInput}
-        />
+        <Controls createTransaction={this.createTransaction} />
         <Balance transactions={transactions} balance={balance} />
         <TaransactionHistory transactions={transactions} />
         <ToastContainer />
